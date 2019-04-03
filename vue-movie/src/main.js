604195c88ec663f90267d06979afccd3c37589f3
HappyDoubleKang <467455219@qq.com>
2019-04-03 12:33:30 +0800

    update

--- vue-movie/src/main.js
+++ vue-movie/src/main.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store/index'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(ElementUI);
 
 // 请求绑定Vue原型属性上面，方便使用
 Vue.prototype.$http = axios;
